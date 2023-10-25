47f73205332ff870f96d58e519ec7109a44c2cd4
NikChist88 <NikChistyakov88@yandex.ru>
2023-10-25 23:21:39 +0300

    start project anima

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components'
+// import './fonts.css'
+
+export const GlobalStyles = createGlobalStyle`
+* {
+  padding: 0px;
+  margin: 0px;
+  border: 0px;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+
+a {
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: 'Poppins', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* font-size: calc((100vw - 320px) / (1920 - 320) * (18 - 14) + 14px); */
+}
+`
