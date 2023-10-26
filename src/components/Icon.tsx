7b443a9c7f0912b21b7886847460e3865ea14f5f
NikChist88 <NikChistyakov88@yandex.ru>
2023-10-26 13:23:59 +0300

    Let's get started!!!

---
+++ src/components/Icon.tsx
@@ -0,0 +1,25 @@
+import sprite from '../assets/icons/sprite.svg'
+
+type IconPropsType = {
+  iconId: string
+  width?: string
+  height?: string
+  viewbox?: string
+  fill?: string
+  stroke?: string
+}
+
+export const Icon = (props: IconPropsType) => {
+  return (
+    <svg
+      width={props.width}
+      height={props.height}
+      viewBox={props.viewbox}
+      fill={props.fill}
+      stroke={props.stroke}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <use xlinkHref={`${sprite}#${props.iconId}`}></use>
+    </svg>
+  )
+}
--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from 'styled-components'
-// import './fonts.css'
+import './fonts.css'
 
 export const GlobalStyles = createGlobalStyle`
 * {
@@ -23,9 +23,12 @@ ul {
 }
 
 body {
-  font-family: 'Poppins', sans-serif;
+  font-family: 'Raleway', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* font-size: calc((100vw - 320px) / (1920 - 320) * (18 - 14) + 14px); */
+  font-size: calc((100vw - 320px) / (1920 - 320) * (22 - 14) + 14px);
+  color: #ffffff;
+  font-weight: 400;
+  font-style: normal;
 }
 `
